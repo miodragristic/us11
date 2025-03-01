ad913dae58f2e770bf58b05c904c931dd1311bf1
Miodrag Mio Ristic <mioristic@yahoo.com>
2025-03-01 22:48:30 +0100

    Update Inter.jsx

--- src/components/Clubs/Inter.jsx
+++ src/components/Clubs/Inter.jsx
@@ -36,11 +36,22 @@ const TrendingPlayers = () => {
 
 
 const Inter = () => {
-  const items = [
-    { id: 1, title: 'Jerseys', imageUrl: 'path/to/jersey-image.jpg', link: 'https://www.amazon.com/jerseys-inter-miami', price: '$79.99' },
-    { id: 2, title: 'T-Shirts', imageUrl: 'path/to/tshirt-image.jpg', link: 'https://www.amazon.com/tshirts-inter-miami', price: '$29.99' },
-    { id: 3, title: 'Merchandise', imageUrl: 'path/to/merch-image.jpg', link: 'https://www.amazon.com/merchandise-inter-miami', price: '$49.99' },
+    const items = [
+      { id: 1, title: "Men's Inter Miami CF Lionel Messi adidas Light Pink 2025 Euforia Authentic Player Jersey", imageUrl: 'https://images.footballfanatics.com/inter-miami-cf/mens-adidas-lionel-messi-light-pink-inter-miami-cf-2025-euforia-authentic-player-jersey_ss5_p-202860048+u-90c1ap4gywaxy53ptq00+v-yujii65dkxwfdeedytbp.jpg?_hv=2&w=340', link: 'https://mlsstore.i8h2.net/XmNXL3', price: '$194.99' },
+      { id: 2, title: "Youth Inter Miami CF Lionel Messi adidas Light Pink 2025 Euforia Replica Player Jersey", imageUrl: 'https://images.footballfanatics.com/inter-miami-cf/youth-adidas-lionel-messi-light-pink-inter-miami-cf-2025-euforia-replica-player-jersey_ss5_p-201677367+u-hrow2y8rhvpwrddyelhp+v-dftko5xtuap5eeoanmxy.jpg?_hv=2&w=340', link: 'https://mlsstore.i8h2.net/zx42eG', price: '$109.99' },
+      { id: 3, title: "Men's Inter Miami CF Lionel Messi adidas Black 2025 The Fortitude Kit Authentic Player Jersey", imageUrl: 'https://images.footballfanatics.com/inter-miami-cf/mens-adidas-lionel-messi-black-inter-miami-cf-2025-the-fortitude-kit-authentic-player-jersey_ss5_p-202594033+u-knqnh7w92m4datnj7vqo+v-ouhd9owk3afqcevad5fn.jpg?_hv=2&w=340', link: 'https://mlsstore.i8h2.net/Oebm9P', price: '$194.99' },
+      { id: 4, title: "Youth Inter Miami CF Lionel Messi adidas Black 2025 The Fortitude Kit Replica Player Jersey", imageUrl: 'https://images.footballfanatics.com/inter-miami-cf/youth-adidas-lionel-messi-black-inter-miami-cf-2025-the-fortitude-kit-replica-player-jersey_ss5_p-201604870+u-mgihjk9ywffxfwjtkbrl+v-fn0trzdceyzwsnzaj1op.jpg?_hv=2&w=340', link: 'https://mlsstore.i8h2.net/vPVZ4L', price: '$109.99' },
+      { id: 5, title: "Men's Inter Miami CF Lionel Messi adidas Black 2025 The Fortitude Kit Replica Player Jersey", imageUrl: 'https://images.footballfanatics.com/inter-miami-cf/mens-adidas-lionel-messi-black-inter-miami-cf-2025-the-fortitude-kit-replica-player-jersey_ss5_p-201604868+u-wb8lncyovo25ie3w7gxq+v-68593kkmocwzvc55q9tq.jpg?_hv=2&w=340', link: 'https://mlsstore.i8h2.net/yqkmeG', price: '$129.99' },
+      { id: 6, title: "Men's Inter Miami CF Lionel Messi adidas Light Pink 2025 Euforia Replica Player Jersey", imageUrl: 'https://images.footballfanatics.com/inter-miami-cf/mens-adidas-lionel-messi-light-pink-inter-miami-cf-2025-euforia-replica-player-jersey_ss5_p-201677369+u-q7pgp0gh0zui6ksxdfmy+v-tv9ine8f7ynydvoxqofb.jpg?_hv=2&w=340', link: 'https://mlsstore.i8h2.net/mOG22O', price: '$129.99' },
+      { id: 7, title: "Men's Inter Miami CF Lionel Messi adidas Aqua 2024 Archive Authentic Jersey", imageUrl: 'https://images.footballfanatics.com/inter-miami-cf/mens-adidas-lionel-messi-aqua-inter-miami-cf-2024-archive-authentic-jersey_ss5_p-201718913+u-wsqzu0wcriatez9xey7h+v-3wnoyn7wipn1tfxkry9z.jpg?_hv=2&w=340', link: 'https://mlsstore.i8h2.net/qzy22g', price: '$129.99' },
+      { id: 8, title: "Men's Inter Miami CF Lionel Messi adidas Pink 2024 2getherness 2024 2getherness Authentic Player Jersey", imageUrl: 'https://images.footballfanatics.com/inter-miami-cf/mens-adidas-lionel-messi-pink-inter-miami-cf-2024-2getherness-2024-2getherness-authentic-player-jersey_ss5_p-201189575+u-9kfkaf3zfug6vvnjhtfv+v-ilisv1besbmmekk2h8py.jpg?_hv=2&w=340', link: 'https://mlsstore.i8h2.net/xLZNNy', price: '$89.99' },
+      { id: 9, title: "Women's Inter Miami CF Lionel Messi adidas Light Pink 2025 Euforia Replica Player Jersey", imageUrl: 'https://images.footballfanatics.com/inter-miami-cf/womens-adidas-lionel-messi-light-pink-inter-miami-cf-2025-euforia-replica-player-jersey_ss5_p-201677368+u-m2kf9ujhva77tepnux4c+v-jeopznh8m1bpu3weowcv.jpg?_hv=2&w=340', link: 'https://mlsstore.i8h2.net/Dy3kkb', price: '$129.99' },
+      { id: 10, title: "Men's Inter Miami CF adidas Pink Home 2025 Euforia Authentic Shorts", imageUrl: 'https://images.footballfanatics.com/inter-miami-cf/mens-adidas-lionel-messi-pink-inter-miami-cf-2024-2getherness-replica-player-jersey_ss5_p-201141929+u-gvf4do2tnc53azskbwn8+v-euq3yy4duk9it86xolnx.jpg?_hv=2&w=340', link: 'https://mlsstore.i8h2.net/Qjb666', price: '$59.99' },
+      { id: 11, title: "Men's Inter Miami CF Lionel Messi adidas Black 2024 La Noche Replica Player Jersey", imageUrl: 'https://images.footballfanatics.com/inter-miami-cf/mens-adidas-lionel-messi-black-inter-miami-cf-2024-la-noche-replica-player-jersey_ss5_p-201141932+u-jxtq9fqkslstjtehu4iu+v-gafrtmumts55sro0vllg.jpg?_hv=2&w=340', link: 'https://mlsstore.i8h2.net/jemOOZ', price: '$59.99' },
+      { id: 12, title: "Men's Inter Miami CF Lionel Messi Black Authentic Stack Name & Number T-Shirt", imageUrl: 'https://images.footballfanatics.com/inter-miami-cf/mens-lionel-messi-black-inter-miami-cf-authentic-stack-name-and-number-t-shirt_ss5_p-202772322+u-ejgjhlhnzgtpxpyjqdnb+v-t89wvstyj4iybc5rkui3.png?_hv=2&w=340', link: 'https://mlsstore.i8h2.net/kOWooV', price: '$31.99 with code' },
+      { id: 13, title: "Men's Inter Miami CF adidas Black 2025 Travel Pullover Hoodie", imageUrl: 'https://images.footballfanatics.com/inter-miami-cf/mens-adidas-black-inter-miami-cf-2025-travel-pullover-hoodie_ss5_p-201657333+u-xmuye01hrwmgdllpcmaw+v-ba7khhpbx9yqateirfku.jpg?_hv=2&w=340', link: 'https://mlsstore.i8h2.net/LK2ZZo', price: '$79.99' },
   ];
+  
 
   return (
     <div className='w-full min-h-screen bg-white pt-[150px]'>
@@ -110,29 +121,28 @@ const Inter = () => {
         <TrendingPlayers />
 
         {/* Shop Section */}
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#020617] mb-4'>Shop</h1>
-        <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-          {items.map(item => (
-            <div key={item.id} className='group rounded-lg overflow-hidden shadow-md hover:shadow-xl transition duration-300'>
-              <div className='relative overflow-hidden bg-gray-200 h-48'>
-                <img src={item.imageUrl} alt={item.title} className='object-contain w-full h-full group-hover:scale-105 transition duration-300' />
-              </div>
-              <div className='p-4'>
-                <h3 className='text-xl font-bold group-hover:text-blue-600 transition duration-300'>{item.title}</h3>
-                <p className='text-lg font-semibold'>{item.price}</p>
-                <a href={item.link} target='_blank' rel='noopener noreferrer' className='block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300 mt-2'>
-                  Buy on Amazon <FaShoppingCart className='inline-block ml-2' />
-                </a>
+               <h1 className='text-4xl sm:text-7xl font-bold text-[#020617] mb-4'>Shop Messi's Merchandise</h1>
+                <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                  {items.map(item => (
+                    <div key={item.id} className='group rounded-lg overflow-hidden shadow-md hover:shadow-xl transition duration-300'>
+                      <div className='relative overflow-hidden bg-gray-200 h-64'>
+                        <img src={item.imageUrl} alt={item.title} className='object-contain w-full h-full group-hover:scale-105 transition duration-300' />
+                      </div>
+                      <div className='p-4'>
+                        <h3 className='text-lg font-bold group-hover:text-blue-600 transition duration-300'>{item.title}</h3>
+                        <p className='text-sm font-semibold'>{item.price}</p>
+                        <a href={item.link} target='_blank' rel='noopener noreferrer' className='block bg-blue-500 cursor-pointer hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300 mt-2'>
+                          Buy on MLS Store <FaShoppingCart className='inline-block ml-2' />
+                        </a>
+                      </div>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </div>
   );
 };
 
 export default Inter;
-
 
 
