b7571871c21d07118d183105a1bfcc5ac2af42d9
Miodrag Mio Ristic <mioristic@yahoo.com>
2025-04-25 01:59:05 +0300

    Update TopMlsJersey2024.jsx

--- src/components/Blog/TopMlsJersey2024.jsx
+++ src/components/Blog/TopMlsJersey2024.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const TopMLSJerseys2024 = () => {
   const shopItemsByPlayerId = {
@@ -161,6 +162,21 @@ const TopMLSJerseys2024 = () => {
 
   return (
     <div className='w-full min-h-screen bg-white'>
+      <Helmet>
+        {/* Open Graph metatage za Facebook */}
+        <meta property="og:title" content="Top 10 MLS Jerseys of 2024: Style, Passion & a Bit of Magic" />
+        <meta property="og:description" content="Explore the top MLS jerseys of 2024, featuring iconic players like Lionel Messi, Luis Suárez, and more. Get the perfect jersey for every fan." />
+        <meta property="og:image" content="https://images.supersport.com/media/0adn4b1p/lionel-messi-24-12-g-1200.jpg?width=1920&quality=90&format=webp" />
+        <meta property="og:url" content={window.location.href} />
+        <meta property="og:type" content="article" />
+
+        {/* Twitter Card metatage */}
+        <meta name="twitter:title" content="Top 10 MLS Jerseys of 2024: Style, Passion & a Bit of Magic" />
+        <meta name="twitter:description" content="Explore the top MLS jerseys of 2024, featuring iconic players like Lionel Messi, Luis Suárez, and more. Get the perfect jersey for every fan." />
+        <meta name="twitter:image" content="https://images.supersport.com/media/0adn4b1p/lionel-messi-24-12-g-1200.jpg?width=1920&quality=90&format=webp" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Helmet>
+
       <nav className='bg-gray-800 py-4'>
         <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
           <div className='relative flex items-center justify-between h-16'>
@@ -172,6 +188,7 @@ const TopMLSJerseys2024 = () => {
           </div>
         </div>
       </nav>
+
 
       <div className='max-w-4xl mx-auto py-8 px-4 md:px-0'>
         <h1 className='text-4xl sm:text-6xl font-bold text-[#020617] mb-8 mt-16 lg:mt-10'>
